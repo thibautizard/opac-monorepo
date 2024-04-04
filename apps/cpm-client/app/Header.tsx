7377eb752a6ec03111d277d006a8e354bea05be2
Thibaut Izard <thibautizard@macbook-pro-de-thibaut.home>
2024-04-04 17:35:03 +0200

    chore: set up shadcn for monorepo

--- apps/cpm-client/app/Header.tsx
+++ apps/cpm-client/app/Header.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import { Button } from "@repo/ui";
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+	return (
+		<>
+			<div>Header</div>
+			<Button>Click me</Button>
+			<br></br>
+			<button>Click me</button>
+		</>
+	);
 }
--- apps/cpm-client/tailwind.config.ts
+++ apps/cpm-client/tailwind.config.ts
@@ -1,10 +1,8 @@
 // tailwind config is required for editor support
-
-import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx"],
+const config: any = {
+  content: ["./app/**/*.tsx", "../../packages/ui/src/**/*.tsx"],
   presets: [sharedConfig],
 };
 
