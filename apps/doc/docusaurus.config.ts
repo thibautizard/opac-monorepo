a6daa7c328dbd20ef5e5a1d217be8ebb345354dc
Thibaut Izard <thibaut.izard@gmail.com>
2024-07-17 18:23:31 +0200

    ✨ feat(doc): Add doc app (#1)

---
+++ apps/doc/docusaurus.config.ts
@@ -0,0 +1,83 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Documentation for OPAC - Flow",
+  tagline: "",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "http://www.google.fr",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "OnePark", // Usually your GitHub org/user name.
+  projectName: "opac-flow", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"]
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+        },
+        theme: {
+          customCss: "./src/css/custom.css"
+        }
+      } satisfies Preset.Options
+    ]
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    navbar: {
+      title: "Flow Documentation",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg"
+      },
+      items: [
+        {
+          href: "https://github.com/Onepark/opac-front",
+          label: "GitHub",
+          position: "right"
+        }
+      ]
+    },
+    footer: {
+      style: "light",
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} OnePark`
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula
+    },
+    colorMode: {
+      defaultMode: "light",
+      disableSwitch: true,
+      respectPrefersColorScheme: false
+    }
+  } satisfies Preset.ThemeConfig
+};
+
+export default config;
